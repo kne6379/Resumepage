49242d1a600721f6e582c57b86a30f7ad7d8f432
Robbie <Robbie@email.com>
2024-05-27 19:36:31 +0900

    feat: prisma 모델 구현, 데이터베이스 연결

--- src/app.js
+++ src/app.js
@@ -3,12 +3,17 @@
 import express from "express";
 import cookieParser from "cookie-parser";
 import { SERVER_PORT } from "./constants/env.constant.js";
+import { errorHandler } from "./middlewares/error-handler.middleware.js";
+import { prisma } from "./utils/prisma.util.js";
+import { Prisma } from "@prisma/client";
 
 const app = express();
 const PORT = SERVER_PORT;
 
 app.use(express.json());
 app.use(cookieParser());
+
+app.use(errorHandler);
 
 app.listen(PORT, () => {
   console.log(PORT, "포트로 서버가 열렸어요!");
